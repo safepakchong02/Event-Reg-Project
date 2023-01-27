e186ff1581fb2704d4590fe997a93200ed5ca7b2
imax2600 <lastation001@outlook.com>
2023-01-27 21:15:58 +0700

    ev

--- main/model/event/query_event.php
+++ main/model/event/query_event.php
@@ -1,4 +1,7 @@
- <?php
+<?php
+use Nette\Utils\Random;
+
+    date_default_timezone_set('Asia/Bangkok');
 
     $value = $_GET['event_view'] ?? $_POST['event_view'] ?? null;
     if ($value === null) {
@@ -8,244 +11,289 @@
     include_once('../../../asset/config/config.php');
 
     switch ($value) {
-        case "getEvent" : getEvent(); break;
-        case "createEvent" : createEvent(); break;
-        case "editEvent" : editEvent(); break;
-        case "deleteEvent" : deleteEvent(); break;
-        default: echo "{\"status\": 400 Bad request}";
+        case "getEvent":
+
+
+            getEvent();
+        break;
+        case "createEvent":
+            $userId = $_POST['u_userId']; //CHAR(36) uuid
+            $eventId = genEventId();
+            $title = $_POST['ev_title']; //VARCHAR
+            $detail = $_POST['ev_detail']; //VARCHAR
+            $limit = $_POST['ev_limit']; //INT
+            $dType = $_POST['ev_dType']; //VARCHAR
+            $selfReg = $_POST['ev_selfReg']; //INT
+            $preReg = $_POST['ev_preReg']; //INT
+            $gps = $_POST['ev_gps']; //INT
+            $lat = $_POST['ev_lat']; //DOUBLE
+            $long = $_POST['ev_long']; //DOUBLE
+            $preRegStart = $_POST['ev_preRegStart']; //DATETIME
+            $preRegEnd = $_POST['ev_preRegEnd']; //DATETIME
+            $checkInStart = $_POST['ev_checkInStart']; //DATETIME
+            $checkInEnd = $_POST['ev_checkInEnd']; //DATETIME
+            $eventStart = $_POST['ev_eventStart']; //DATETIME
+            $eventEnd = $_POST['ev_eventEnd']; //DATETIME
+
+            createEvent($userId, $eventId, $title, $detail, $limit, $dType, $selfReg, $preReg, $gps
+            , $lat, $long, $preRegStart, $preRegEnd, $checkInStart, $checkInEnd, $eventStart
+            , $eventEnd);
+        break;
+        case "editEvent":
+            $eventId = $_POST['ev_eventId'];
+            $title = $_POST['ev_title'];
+            $detail = $_POST['ev_detail'];
+            $limit = $_POST['ev_limit'];
+            $dType = $_POST['ev_dType'];
+            $selfReg = $_POST['ev_selfReg'];
+            $preReg = $_POST['ev_preReg'];
+            $gps = $_POST['ev_gps'];
+            $lat = $_POST['ev_lat'];
+            $long = $_POST['ev_long'];
+            $preRegStart = $_POST['ev_preRegStart'];
+            $preRegEnd = $_POST['ev_preRegEnd'];
+            $checkInStart = $_POST['ev_checkInStart'];
+            $checkInEnd = $_POST['ev_checkInEnd'];
+            $eventStart = $_POST['ev_eventStart'];
+            $eventEnd = $_POST['ev_eventEnd'];
+            $status = $_POST['ev_status'];
+            editEvent($eventId, $title, $detail, $limit, $dType, $selfReg, $preReg, $gps
+            , $lat, $long, $preRegStart, $preRegEnd, $checkInStart, $checkInEnd, $eventStart
+            , $eventEnd, $status);
+        break;
+        case "deleteEvent":
+            $eventId = $_POST['ev_eventId'];
+            deleteEvent($eventId);
+        break;
+        default:
+        echo "{\"status\": 400 Bad request}";
     }
 
-    function getEvent() {
+    function genEventId() {
+        $eventId = "EV" + random_bytes(4);
+        $result = mysqli_query($handle, "SELECT ev_eventId from event where ev_eventId = " . $eventId . "");
+        if (!$result) {
+            return $eventId
+        }
+        return genEventId()
+    }
+
+    function getEvent()
+    {
 
     }
 
-    function createEvent() {
-        $mysqli = new mysqli($handle)
-        $mysqli->begin_transaction();
+    function createEvent($userId, $eventId, $title, $detail, $limit, $dType, $selfReg, $preReg, $gps
+    , $lat, $long, $preRegStart, $preRegEnd, $checkInStart, $checkInEnd, $eventStart
+    , $eventEnd)
+    {
+
+        $handle->begin_transaction();
         try {
-            $query = "CALL createEvent( " . $eventId ."  )"
+            $query = "CALL createEvent( " . $userId . ", " . $eventId . " " . $title . ", 
+            " . $detail . ", " . $limit . ", 
+            " . $dType . ", " . $selfReg . ", 
+            " . $preReg . ", " . $gps . ",
+            " . $lat . ", " . $long . ", 
+            " . $preRegStart . ", " . $preRegEnd . ", 
+            " . $checkInStart . ", " . $checkInEnd . ", 
+            " . $eventStart . ", " . $eventEnd . ")";
 
-            $result = mysqli_query($mysqli, $query);
+            $result = mysqli_query($handle, $query);
 
             if ($result > 0) {
                 echo "{\"status\": 200 Success}";
             }
             echo "{\"status\": 400 Bad request}";
-            $mysqli->commit();
+            $handle->commit();
         }
-        catch (Exception e) {
-
+        catch (Exception $e) {
+            $handle->rollback();
+            echo $e->getMessage();
         }
-        $mysqli->close();
+        $handle->close();
     }
 
-    function editEvent() {
-        $mysqli = new mysqli($handle)
-        $mysqli->begin_transaction();
+    function editEvent($eventId, $title, $detail, $limit, $dType, $selfReg, $preReg, $gps
+    , $lat, $long, $preRegStart, $preRegEnd, $checkInStart, $checkInEnd, $eventStart
+    , $eventEnd, $status)
+    {
+        $handle->begin_transaction();
         try {
-            $query = "CALL editEvent( " . $eventId ."  )"
+            $query = "CALL editEvent( " . $eventId . ", " . $title . ", 
+            " . $detail . ", " . $limit . ", 
+            " . $dType . ", " . $selfReg . ", 
+            " . $preReg . ", " . $gps . ",
+            " . $lat . ", " . $long . ", 
+            " . $preRegStart . ", " . $preRegEnd . ", 
+            " . $checkInStart . ", " . $checkInEnd . ", 
+            " . $eventStart . ", " . $eventEnd . ", ". $status . ")";
 
-            $result = mysqli_query($mysqli, $query);
+            $result = mysqli_query($handle, $query);
 
             if ($result > 0) {
                 echo "{\"status\": 200 Success}";
             }
             echo "{\"status\": 400 Bad request}";
-            $mysqli->commit();
+            $handle->commit();
         }
-        catch (Exception e) {
-
+        catch (Exception $e) {
+            $handle->rollback();
+            echo $e->getMessage();
         }
-        $mysqli->close();
+        $handle->close();
     }
 
-    function deleteEvent($eventId) {
-        $mysqli = new mysqli($handle)
-        $mysqli->begin_transaction();
+    function deleteEvent($eventId)
+    {
+        $handle->begin_transaction();
         try {
-            $query = "CALL deleteEvent( " . $eventId ."  )"
+            $query = "CALL deleteEvent( " . $eventId . "  )";
 
-            $result = mysqli_query($mysqli, $query);
+            $result = mysqli_query($handle, $query);
 
             if ($result > 0) {
                 echo "{\"status\": 200 Success}";
             }
             echo "{\"status\": 400 Bad request}";
-            $mysqli->commit();
+            $handle->commit();
         }
-        catch (Exception e) {
+        catch (Exception $e) {
 
         }
-        $mysqli->close();
+        $handle->close();
     }
 
-    date_default_timezone_set('Asia/Bangkok');
+/*if (@$_GET['event_view'] != '') {
+ $event_view = @$_GET['event_view']; } else if (@$_POST['event_view'] != '') {
+ $event_view = @$_POST['event_view']; } else {
+ $event_view = ''; } include_once('../../../asset/config/config.php');
+ //******************** show data *********************/
+/*if (@$_SESSION["perm"] == "") {
+ echo "{\"status\": 403}"; } else if ($event_view == 'show_data') {
+ if ($_SESSION["perm"] == "admin") {
+ $sql = "SELECT DISTINCT ev_id, ev_title, ev_date_start, ev_date_end, `user_name`, `user_surname` FROM events " .
+ "LEFT JOIN users " .
+ "ON events.ev_assign_to = users.user_id " .
+ "WHERE ev_del = '0'";
+ } elseif ($_SESSION["perm"] == "register") {
+ $sql = "SELECT DISTINCT ev_id, ev_title, ev_date_start, ev_date_end, `user_name`, `user_surname` FROM events " .
+ "LEFT JOIN users " .
+ "ON events.ev_assign_to = users.user_id " .
+ "WHERE ev_del = '0' and ev_assign_to = '" . $_SESSION["user_id"] . "'";
+ } else {
+ $sql = "SELECT DISTINCT ev_id, ev_title, ev_date_start, ev_date_end, `user_name`, `user_surname` FROM events " .
+ "LEFT JOIN users " .
+ "ON events.ev_assign_to = users.user_id " .
+ "WHERE ev_del = '0' and ev_create_by = '" . $_SESSION["user_id"] . "'";
+ }
+ $resource_data = mysqli_query($handle, $sql);
+ $count_row = mysqli_num_rows($resource_data);
+ if ($count_row > 0) {
+ while ($result = mysqli_fetch_assoc($resource_data)) {
+ $js["ev_id"] = $result["ev_id"];
+ $js["ev_title"] = $result["ev_title"];
+ $js["ev_date_start"] = $result["ev_date_start"];
+ $js["ev_date_end"] = $result["ev_date_end"];
+ $js["user_name"] = $result["user_name"];
+ $js["user_surname"] = $result["user_surname"];
+ $d_st = DateTime::createFromFormat('d/m/Y H:i', $result["ev_date_start"]);
+ $d_ed = DateTime::createFromFormat('d/m/Y H:i', $result["ev_date_end"]);
+ $now = new DateTime();
+ if ($now >= $d_ed) {
+ $js["ev_status"] = "ปิดลงทะเบียน";
+ $js["isOpen"] = "table-danger";
+ } elseif ($now >= $d_st) {
+ $js["ev_status"] = "เปิดลงทะเบียน";
+ $js["isOpen"] = "table-success";
+ } else {
+ $js["ev_status"] = "รอเปิดลงทะเบียน";
+ $js["isOpen"] = "table-warning";
+ }
+ $rows[] = $js;
+ // $rows[] = $result;
+ }
+ $data = json_encode($rows);
+ //$totaldata = sizeof($rows);
+ $results = '{"results_data":' . $data . '}';
+ }
+ echo $results;
+ //******************** add data *********************/
+/*} else if ($event_view == 'add') {
+ $ev_id = 1;
+ // หาไอดีที่มากที่สุด
+ $sql_max_val = "SELECT MAX(ev_id) FROM `events`";
+ $rs = mysqli_query($handle, $sql_max_val);
+ $count_row = mysqli_num_rows($rs);
+ if ($count_row > 0) {
+ while ($result = mysqli_fetch_assoc($rs)) {
+ $ev_id = intval($result["MAX(ev_id)"]) + 1;
+ } // end while
+ } // end if
+ //sql insert data
+ $sql_add_event = "insert into events set " .
+ "ev_id='" . $ev_id . "'," .
+ "ev_title='" . $_POST['ev_title'] . "'," .
+ "ev_assign_to='" . $_POST['ev_assign_to'] . "'," .
+ "ev_date_start='" . $_POST['ev_date_start'] . "'," .
+ "ev_date_end='" . $_POST['ev_date_end'] . "'," .
+ "ev_create_by='" . $_SESSION["user_id"] . "'," .
+ "ev_del = '0';";
+ mysqli_query($handle, $sql_add_event);
+ // echo $sql_add_event;
+ //******************** add data to table *********************//*
+ $sql_add_data = "INSERT INTO `event_setting` SET " .
+ "`ev_id` = '" . $ev_id . "'," .
+ "`walk_in` = '1'," .
+ "`emp_id` = '0'," .
+ "`card_id` = '0'," .
+ "`name` = '0'," .
+ "`call` = '0'," .
+ "`com_name` = '0'," .
+ "`dep` = '0'," .
+ "`pos` = '0'," .
+ "`salary` = '0'," .
+ "`gender` = '0'," .
+ "`age` = '0'," .
+ "`birthDate` = '0' ";
+ mysqli_query($handle, $sql_add_data);
+ //******************** view Edit *********************//* } else if ($event_view == 'show_data_edit') {
+ $sql = "SELECT * FROM `events`" .
+ " WHERE ev_id = '" . $_GET['ev_id'] . "'";
+ $resource_data = mysqli_query($handle, $sql);
+ $numRows = mysqli_num_fields($resource_data);
+ //		$resultArray = array();
+ while ($result = mysqli_fetch_assoc($resource_data)) {
+ $rows[] = $result;
+ }
+ $data = json_encode($rows);
+ $results = '{"results_data_edit":' . $data . '}';
+ echo $results;
+ //******************** Update Edit *********************//* } else if ($event_view == 'edit_form_save') {
+ if ($_POST['ev_id'] != '') {
+ $ev_id = $_POST["ev_id"];
+ $ev_title = $_POST['ev_title'];
+ $ev_date_start = $_POST['ev_date_start'];
+ $ev_date_end = $_POST['ev_date_end'];
+ $ev_assign_to = $_POST['ev_assign_to'];
+ $sql_update = "UPDATE `events` SET `ev_title` = '$ev_title'," .
+ " `ev_assign_to` = '$ev_assign_to'," .
+ " `ev_date_start` = '$ev_date_start'," .
+ " `ev_date_end` = '$ev_date_end'" .
+ " WHERE `ev_id` = '$ev_id'";
+ mysqli_query($handle, $sql_update);
+ // echo $sql_update;
+ }
+ //******************** delete data *********************//* } else if ($event_view == 'del_event') {
+ if ($_GET['ev_id'] != '') {
+ $ev_id = $_GET['ev_id'];
+ $sql_delete_event = "UPDATE `events` SET `ev_del` = '1'" .
+ " WHERE `events`.`ev_id` = '$ev_id'";
+ mysqli_query($handle, $sql_delete_event);
+ // echo $sql_delete_event;
+ }
+ //******************** else *********************//* } else {
+ $results = '{"results_data":null}';
+ echo $results; } */
 
-    if (@$_GET['event_view'] != '') {
-        $event_view = @$_GET['event_view'];
-    } else if (@$_POST['event_view'] != '') {
-        $event_view = @$_POST['event_view'];
-    } else {
-        $event_view = '';
-    }
-    include_once('../../../asset/config/config.php');
-
-    //******************** show data *********************//
-    if (@$_SESSION["perm"] == "") {
-        echo "{\"status\": 403}";
-    } else if ($event_view == 'show_data') {
-
-        if ($_SESSION["perm"] == "admin") {
-            $sql = "SELECT DISTINCT ev_id, ev_title, ev_date_start, ev_date_end, `user_name`, `user_surname` FROM events " .
-                "LEFT JOIN users " .
-                "ON events.ev_assign_to = users.user_id " .
-                "WHERE ev_del = '0'";
-        } elseif ($_SESSION["perm"] == "register") {
-            $sql = "SELECT DISTINCT ev_id, ev_title, ev_date_start, ev_date_end, `user_name`, `user_surname` FROM events " .
-                "LEFT JOIN users " .
-                "ON events.ev_assign_to = users.user_id " .
-                "WHERE ev_del = '0' and ev_assign_to = '" . $_SESSION["user_id"] . "'";
-        } else {
-            $sql = "SELECT DISTINCT ev_id, ev_title, ev_date_start, ev_date_end, `user_name`, `user_surname` FROM events " .
-                "LEFT JOIN users " .
-                "ON events.ev_assign_to = users.user_id " .
-                "WHERE ev_del = '0' and ev_create_by = '" . $_SESSION["user_id"] . "'";
-        }
-
-        $resource_data = mysqli_query($handle, $sql);
-        $count_row = mysqli_num_rows($resource_data);
-
-        if ($count_row > 0) {
-            while ($result = mysqli_fetch_assoc($resource_data)) {
-                $js["ev_id"] = $result["ev_id"];
-                $js["ev_title"] = $result["ev_title"];
-                $js["ev_date_start"] = $result["ev_date_start"];
-                $js["ev_date_end"] = $result["ev_date_end"];
-                $js["user_name"] = $result["user_name"];
-                $js["user_surname"] = $result["user_surname"];
-
-                $d_st = DateTime::createFromFormat('d/m/Y H:i', $result["ev_date_start"]);
-                $d_ed = DateTime::createFromFormat('d/m/Y H:i', $result["ev_date_end"]);
-                $now = new DateTime();
-
-                if ($now >= $d_ed) {
-                    $js["ev_status"] = "ปิดลงทะเบียน";
-                    $js["isOpen"] = "table-danger";
-                } elseif ($now >= $d_st) {
-                    $js["ev_status"] = "เปิดลงทะเบียน";
-                    $js["isOpen"] = "table-success";
-                } else {
-                    $js["ev_status"] = "รอเปิดลงทะเบียน";
-                    $js["isOpen"] = "table-warning";
-                }
-
-                $rows[] = $js;
-                // $rows[] = $result;
-            }
-
-            $data = json_encode($rows);
-            //$totaldata = sizeof($rows);
-            $results = '{"results_data":' . $data . '}';
-        }
-        echo $results;
-
-        //******************** add data *********************//
-    } else if ($event_view == 'add') {
-        $ev_id = 1;
-
-        // หาไอดีที่มากที่สุด
-        $sql_max_val = "SELECT MAX(ev_id) FROM `events`";
-        $rs = mysqli_query($handle, $sql_max_val);
-        $count_row = mysqli_num_rows($rs);
-
-        if ($count_row > 0) {
-            while ($result = mysqli_fetch_assoc($rs)) {
-                $ev_id = intval($result["MAX(ev_id)"]) + 1;
-            } // end while
-        } // end if
-
-        //sql insert data
-        $sql_add_event = "insert into events set " .
-            "ev_id='" . $ev_id . "'," .
-            "ev_title='" . $_POST['ev_title'] . "'," .
-            "ev_assign_to='" . $_POST['ev_assign_to'] . "'," .
-            "ev_date_start='" . $_POST['ev_date_start'] . "'," .
-            "ev_date_end='" . $_POST['ev_date_end'] . "'," .
-            "ev_create_by='" . $_SESSION["user_id"] . "'," .
-            "ev_del = '0';";
-
-        mysqli_query($handle, $sql_add_event);
-        // echo $sql_add_event;
-
-        //******************** add data to table *********************//
-        $sql_add_data = "INSERT INTO `event_setting` SET " .
-            "`ev_id` = '" . $ev_id . "'," .
-            "`walk_in` = '1'," .
-            "`emp_id` = '0'," .
-            "`card_id` = '0'," .
-            "`name` = '0'," .
-            "`call` = '0'," .
-            "`com_name` = '0'," .
-            "`dep` = '0'," .
-            "`pos` = '0'," .
-            "`salary` = '0'," .
-            "`gender` = '0'," .
-            "`age` = '0'," .
-            "`birthDate` = '0' ";
-        mysqli_query($handle, $sql_add_data);
-
-        //******************** view Edit *********************//
-    } else if ($event_view == 'show_data_edit') {
-
-        $sql = "SELECT * FROM `events`" .
-            " WHERE ev_id = '" . $_GET['ev_id'] . "'";
-        $resource_data = mysqli_query($handle, $sql);
-        $numRows = mysqli_num_fields($resource_data);
-        //		$resultArray = array();
-
-        while ($result = mysqli_fetch_assoc($resource_data)) {
-            $rows[] = $result;
-        }
-
-        $data = json_encode($rows);
-        $results = '{"results_data_edit":' . $data . '}';
-        echo $results;
-
-        //******************** Update Edit *********************//
-    } else if ($event_view == 'edit_form_save') {
-        if ($_POST['ev_id'] != '') {
-            $ev_id = $_POST["ev_id"];
-            $ev_title = $_POST['ev_title'];
-            $ev_date_start = $_POST['ev_date_start'];
-            $ev_date_end = $_POST['ev_date_end'];
-            $ev_assign_to = $_POST['ev_assign_to'];
-
-
-            $sql_update = "UPDATE `events` SET `ev_title` = '$ev_title'," .
-                " `ev_assign_to` = '$ev_assign_to'," .
-                " `ev_date_start` = '$ev_date_start'," .
-                " `ev_date_end` = '$ev_date_end'" .
-                " WHERE `ev_id` = '$ev_id'";
-            mysqli_query($handle, $sql_update);
-            // echo $sql_update;
-        }
-
-        //******************** delete data *********************//
-    } else if ($event_view == 'del_event') {
-
-        if ($_GET['ev_id'] != '') {
-            $ev_id = $_GET['ev_id'];
-            $sql_delete_event = "UPDATE `events` SET `ev_del` = '1'" .
-                " WHERE `events`.`ev_id` = '$ev_id'";
-            mysqli_query($handle, $sql_delete_event);
-            // echo $sql_delete_event;
-        }
-
-        //******************** else *********************//
-    } else {
-        $results = '{"results_data":null}';
-        echo $results;
-    }
-
-
-    ?>
+?>
